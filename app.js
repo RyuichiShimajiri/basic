d714a749c913ab2d8f716ab4caee0f81c58a2e3a
Ryuichi Shimajiri <>
2020-10-20 20:20:45 +0900

    修復

--- app.js
+++ app.js
@@ -68,19 +68,111 @@ let studentlist =[
  console.log(studentlist[3].from);
  console.log(studentlist[4].hobby);
 
-let sport =[
-  {name:'野球',},
-  {name:'サッカー'},
-  ]
+let weight = 116;
 
-console.log(sport[0].name);
+if (weight >= 200) {
+  console.log('ヘビー級');
+}else if(weight >= 115 && weight <=118){
+  console.log('バンダム級');
+}else{
+  console.log('その他の階級');
+}
 
-let sport[0].name =[
-  {name:'プロ野球'}
-  {name:'草野球'}
-]
 
-console.log(sport[0].name[0].name);
+
+let greeting = 'gskjh';
+
+switch (greeting){
+ case'おはよう':
+  console.log('日本語')
+  break;
+ case 'good morning':
+  console.log('英語');
+  break;
+ default:
+ console.log('?');
+  break;
+}
+
+
+for (let i = 1; i <= 10; i++){
+  console.log('くりかえしますよ');
+  console.log(i);
+}
+
+
+for (let i = 2; i <= 20; i++) {
+  console.log(i);
+}
+
+
+// for文を使ってstudentlist2のname部分を、consoleに表示して下さい。
+// ヒント：forで数が変わるiは、○番目の指定にも使えます。
+let studentlist2 =[
+  {name:'Rioさん',from:'Ginowan City',hobby:'バスケ'},
+  {name:'Katsumiさん',from:'Miyakojima City',hobby:'ゲーム'},
+  {name:'Gokiさん',from:'Uruma City',hobby:'サッカー'},
+  {name:'Michiyoさん',from:'Kanagawa',hobby:'旅行'},
+  {name:'Maikoさん',from:'',hobby:'旅行2'},
+];
+
+console.log(studentlist2 [0] .name);
+console.log(studentlist2 [1] .name);
+console.log(studentlist2 [2] .name);
+console.log(studentlist2 [3] .name);
+console.log(studentlist2 [4] .name);
+
+
+for (let i = 0; i <=4;i++){
+  console.log(studentlist2[i].name);
+}
+
+//1 - 30までの数値で、3の倍数ではないものを表示するプログラム
+//3の倍数の時だけ処理が不要なため、continueでスキップする。
+for (let i = 1; i <= 30; i++) {
+    if (i % 3 === 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+// 以下の条件にあうfor文を書いてください。
+
+// 初期化式で変数iに1を代入する
+// iが50以下の間繰り返す
+// 1回ごとにiを1増やす
+// 繰り返しのたびにiをconsoleに表示する
+// ただし、iが5の倍数でない場合は表示しない。
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 5 === 0){ 
+    console.log(i);
+  }else{
+    continue
+  }
+}
+
+// ランダム変数でおみくじを作ってみよう
+console.log("ランダム変数")
+var random = Math.floor( Math.random() * 6 );
+
+console.log( random );
+
+let omikuji = [
+"Rioさん",
+"Kathumiさん",
+"Gokiさん",
+"Michiyoさん",
+"Maikoさん",
+"Ryuichi"
+];
+
+console.log(omikuji[random]);
+
+
+
+
+
 
 
 
